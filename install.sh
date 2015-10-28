6f9966ba26940763d6779c5b8b884808c38676d2
Russell M. Van Tassell <russell@geekoncall.net>
2015-10-28 13:24:29 -0700

    Changed install.sh environments variable to confdir, which is more accurate.

--- install.sh
+++ install.sh
@@ -3,24 +3,25 @@ set -u
 set -e
 
 hiera=$(puppet config print hiera_config)
-environments="$(puppet config print confdir)/environments"
+confdir="$(puppet config print confdir)/environments"
+
 script_dir=$(dirname $0)
 
 puppet module install zack/r10k
 puppet apply "${script_dir}/configure_r10k.pp"
 r10k deploy environment -pv
 
-if [ -f "${environments}/production/hiera.yaml" ] && [ ! -f "$hiera" ]
+if [ -f "${confdir}/production/hiera.yaml" ] && [ ! -f "$hiera" ]
 then
-  echo "Copying ${environments}/production/hiera.yaml to $hiera"
-  /bin/cp -f "${environments}/production/hiera.yaml" "$hiera"
-elif [ -f "${environments}/production/hiera.yaml" ] && [ -f "$hiera" ]
+  echo "Copying ${confdir}/production/hiera.yaml to $hiera"
+  /bin/cp -f "${confdir}/production/hiera.yaml" "$hiera"
+elif [ -f "${confdir}/production/hiera.yaml" ] && [ -f "$hiera" ]
 then
-  diff "${environments}/production/hiera.yaml" "$hiera" > /dev/null
+  diff "${confdir}/production/hiera.yaml" "$hiera" > /dev/null
   if [ $? -ne 0 ]
   then
     echo "$hiera has changed.  Copying from production and restarting the master." 
-    /bin/cp -f "${environments}/production/hiera.yaml" "$hiera"
+    /bin/cp -f "${confdir}/production/hiera.yaml" "$hiera"
     config_dir=$(puppet config print confdir)
     if [ "$config_dir" == "/etc/puppet" ]
     then
@@ -32,6 +33,6 @@ then
 fi
 if [ -f "$hiera" ]
 then
-  echo "Updating datadir in $hiera to point to $environments"
-  sed -i "s;:datadir.*;:datadir: \"${environments}/%{::environment}/hieradata\";" "$hiera"
+  echo "Updating datadir in $hiera to point to $confdir"
+  sed -i "s;:datadir.*;:datadir: \"${confdir}/%{::environment}/hieradata\";" "$hiera"
 fi
